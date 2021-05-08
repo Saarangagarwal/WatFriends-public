960b1db46a5f42bf2fafe745ecb07e7b23bfc4f6
Saarang Agarwal <agarwalsaarang@yahoo.com>
2021-05-08 12:51:22 -0400

    removed all config keys and passwords

---
+++ config/connectycube.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: "",
+    displayName: "Watfriends",
+    senderID: "",
+    connectyCubeConfig: [
+        {
+            appId: ,
+            authKey: "",
+            authSecret: ""
+        },
+        {
+            chat: {
+                streamManagement: {
+                    enable: true
+                }
+            },
+            debug: {
+                mode: 1
+            } 
+        }
+    ]
+}
